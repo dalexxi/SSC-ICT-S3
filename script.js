10f73d9b735d614fcbd3ff8465b56db5e1a4160b
dalexxi <89869386+dalexxi@users.noreply.github.com>
2023-09-27 10:43:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function toggleAccordion(id) {
     var accordion = document.getElementById(id);
     accordion.classList.toggle('active-accordion');
+
+    //Toggle icon for click
+    var icon = accordion.previousElementSiblin.querySelector('i');
+    icon.classList.toggle('fa-caret-down');
+    icon.classlist.toggle('fa-caret-up');
 }
